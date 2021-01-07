7161b0c0f7cf3b383dbb574300748a364e3b40dc
Cristina Silva <casilva62592@gmail.com>
2021-01-06 23:03:28 -0500

    Abstract the nav ui into its own functional component

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -1,5 +1,26 @@
 import React from 'react';
-// tbh this is a dumb name for a component
+
+// abstracting the nav into its own functional component
+function LanguagesNav(props) {
+    const { selected, onUpdateLanguage } = props; // did this to make sure i understood that it's the same as dowing function LanguagesNav({selected, onUpdateLanguage})
+    const languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python'];
+    return (
+        <ul className="flex-center">
+            {languages.map((language) => (
+                <li key={language}>
+                    <button
+                        className="btn-clear nav-link"
+                        style={language === selected ? { color: 'pink' } : null}
+                        onClick={() => onUpdateLanguage(language)}
+                    >
+                        {language}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
 export default class Popular extends React.Component {
     constructor(props) {
         super(props)
@@ -7,34 +28,24 @@ export default class Popular extends React.Component {
         this.state = {
             selectedLanguage: 'All'
         }
-        this.updateLanguage = this.updateLanguage.bind(this); // make sure this is binded to this component
+        this.updateLanguage = this.updateLanguage.bind(this); // makes sure that the context in which `this` is invoked is in the component
     }
 
     updateLanguage(selectedLanguage) {
-        this.setState({ // setState is what triggers the rerender
+        this.setState({
             selectedLanguage
         })
     }
 
     render() {
-        const languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python']; // An array of all the list items we want in our popular page nav
-
+        const { selectedLanguage } = this.state;
         return (
-            <ul className="flex-center"> {/**className is used because 'class' in JS already exists */}
-                {languages.map((language) => (
-                    // `languages` is the variable we set above
-                    // `language` represents the array item (only a name for now)
-                    <li key={language}>
-                        <button
-                            className="btn-clear nav-link"
-                            style={language === this.state.selectedLanguage ? { color: 'pink' } : null}
-                            onClick={() => this.updateLanguage(language)} // if you don't wrap this in an arrow function, it will invoke the function, but we want to pass a definition
-                        >
-                            {language}
-                        </button>
-                    </li>
-                ))}
-            </ul>
+            <React.Fragment>
+                <LanguagesNav
+                    selected={selectedLanguage}
+                    onUpdateLanguage={this.updateLanguage}
+                />
+            </React.Fragment>
         )
     }
 }
