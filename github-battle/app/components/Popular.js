a3eaa13d7b75aa0560ae3308617b884de76ab73b
Cristina Silva <casilva62592@gmail.com>
2021-01-06 22:51:53 -0500

    Adding state to the nav component to change color when clicked

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -1,6 +1,21 @@
 import React from 'react';
-
+// tbh this is a dumb name for a component
 export default class Popular extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedLanguage: 'All'
+        }
+        this.updateLanguage = this.updateLanguage.bind(this); // make sure this is binded to this component
+    }
+
+    updateLanguage(selectedLanguage) {
+        this.setState({ // setState is what triggers the rerender
+            selectedLanguage
+        })
+    }
+
     render() {
         const languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python']; // An array of all the list items we want in our popular page nav
 
@@ -10,7 +25,11 @@ export default class Popular extends React.Component {
                     // `languages` is the variable we set above
                     // `language` represents the array item (only a name for now)
                     <li key={language}>
-                        <button className="btn-clear nav-link">
+                        <button
+                            className="btn-clear nav-link"
+                            style={language === this.state.selectedLanguage ? { color: 'pink' } : null}
+                            onClick={() => this.updateLanguage(language)} // if you don't wrap this in an arrow function, it will invoke the function, but we want to pass a definition
+                        >
                             {language}
                         </button>
                     </li>
