7f4a8e9a41f7d4a89327c2efb9d3856de11b964a
Cristina Silva <casilva62592@gmail.com>
2021-01-08 16:18:09 -0500

    Translate the API data into the grid UI for the popular page

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
 import { fetchPopularRepos } from '../utils/api';
 
 // abstracting the nav into its own functional component
@@ -28,6 +29,52 @@ function LanguagesNav(props) {
 LanguagesNav.propTypes = {
     selected: PropTypes.string.isRequired,
     onUpdateLanguage: PropTypes.func.isRequired,
+}
+
+function ReposGrid({ repos }) {
+    return (
+        <ul className="grid space-around">
+            {repos.map((repo, index) => {
+                const { name, owner, html_url, stargazers_count, forks, open_issues } = repo; // this is called destructuring
+                const { login, avatar_url } = owner;
+
+                return (
+                    <li key={html_url} className="repo bg-light center-text">
+                        <h4 className="header-lg">
+                            #{index + 1}
+                        </h4>
+                        <img className="avatar"
+                            src={avatar_url}
+                            alt={`Avatar for ${login}`}
+                        />
+                        <h2>
+                            <a href={html_url}>{login}</a>
+                        </h2>
+                        <ul className="card-list">
+                            <li>
+                                <FaUser color='rgb(255, 191, 116)' size={22} />
+                                <a href={`https://github.com/${login}`}>
+                                    {login}
+                                </a>
+                            </li>
+                            <li>
+                                <FaStar color='rgb(255, 215, 0)' size={22} />
+                                {stargazers_count.toLocaleString()} stars
+                            </li>
+                            <li>
+                                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                                {forks.toLocaleString()} forks
+                            </li>
+                            <li>
+                                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                                {open_issues.toLocaleString()} open
+                            </li>
+                        </ul>
+                    </li>
+                )
+            })}
+        </ul>
+    )
 }
 
 export default class Popular extends React.Component {
@@ -122,7 +169,7 @@ export default class Popular extends React.Component {
                 />
                 {this.isLoading() && <p>Loading...</p>}
                 { error && <p>{error}</p>}
-                { repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+                { repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
             </React.Fragment>
         )
     }
