bddf96f4d56d721d6020d720221592f13bbf579f
Cristina Silva <casilva62592@gmail.com>
2021-01-14 20:31:38 -0500

    Decouple the hovering logic into it's own higher order component

--- github-battle/app/components/Tooltip.js
+++ github-battle/app/components/Tooltip.js
@@ -23,48 +23,69 @@ const styles = {
     }
 }
 
-export default class Tooltip extends React.Component {
-    constructor(props) {
-        super(props)
+// we can decouple this hovering logic to it's own higher order component
+// will abstract it out to a `withHover` component
+// export default class Tooltip extends React.Component {
+//     constructor(props) {
+//         super(props)
 
-        this.state = {
-            hovering: false,
-        }
-        this.mouseOver = this.mouseOver.bind(this)
-        this.mouseOut = this.mouseOut.bind(this)
-    }
-    // hovering
-    // whenever a user is hovering over location or company, the tooltip shows
-    mouseOver() { // take in the id of list item we're hovering (hoveringLocation or hoveringCompany)
-        this.setState({
-            hovering: true
-        })
-    }
-    // not hovering
-    mouseOut() {
-        this.setState({
-            hovering: false
-        })
-    }
-    render() {
-        const { hovering } = this.state;
-        const { text, children } = this.props;
-        // Usage
-        // <Tooltip text="User's company">
-        //     <h1>Hover over me!</h1>
-        // </Tooltip>
+//         this.state = {
+//             hovering: false,
+//         }
+//         this.mouseOver = this.mouseOver.bind(this)
+//         this.mouseOut = this.mouseOut.bind(this)
+//     }
+//     // hovering
+//     // whenever a user is hovering over location or company, the tooltip shows
+//     mouseOver() { // take in the id of list item we're hovering (hoveringLocation or hoveringCompany)
+//         this.setState({
+//             hovering: true
+//         })
+//     }
+//     // not hovering
+//     mouseOut() {
+//         this.setState({
+//             hovering: false
+//         })
+//     }
+//     render() {
+//         const { hovering } = this.state;
+//         const { text, children } = this.props;
+//         // Usage
+//         // <Tooltip text="User's company">
+//         //     <h1>Hover over me!</h1>
+//         // </Tooltip>
 
-        return (
-            <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} style={styles.container}>
-                { hovering === true &&
-                    <div style={styles.tooltip}>{text}</div>
-                }
-                {children}
-            </div>
-        )
-    }
+//         return (
+//             <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} style={styles.container}>
+//                 { hovering === true &&
+//                     <div style={styles.tooltip}>{text}</div>
+//                 }
+//                 {children}
+//             </div>
+//         )
+//     }
+// }
+
+function Tooltip({ text, children, hovering }) {
+    return (
+        <div style={styles.container}>
+            { hovering === true &&
+                <div style={styles.tooltip}>{text}</div>
+            }
+            {children}
+        </div>
+    )
+
 }
 
 Tooltip.propTypes = {
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    hovering: PropTypes.bool.isRequired
 }
+
+export default withHover(Tooltip)
+// higher order component is a component
+// takes in another component
+// returns a new component
+// the new component can render the original component
---
+++ github-battle/app/components/withHover.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+// this component is just a function that returns another component
+// takes the original component and dresses it up in it's special logic outfit
+// then spits out the original component with the new logic outfit on it
+// like a dressing room
+export default function withHover(Component, propName = 'hovering') { // propName helps prevent name collisions. 'hovering' will be the default unless specified
+    return class WithHover extends React.Component {
+        constructor(props) {
+            super(props)
+
+            this.state = {
+                hovering: false,
+            }
+            this.mouseOver = this.mouseOver.bind(this)
+            this.mouseOut = this.mouseOut.bind(this)
+        }
+        // hovering
+        // whenever a user is hovering over location or company, the tooltip shows
+        mouseOver() { // take in the id of list item we're hovering (hoveringLocation or hoveringCompany)
+            this.setState({
+                hovering: true
+            })
+        }
+        // not hovering
+        mouseOut() {
+            this.setState({
+                hovering: false
+            })
+        }
+
+        render() {
+            // This basically takes the old component prop situation on line 46 and moves it here
+            const props = {
+                [propName]: this.state.hovering,
+                ...this.props
+            }
+            return (
+                // pass the logic of hovering
+                <div
+                    mouseOut={this.mouseOut}
+                    mouseOver={this.mouseOver}
+                >
+                    {/* return the component we passed in with the hovering prop  */}
+                    {/* we need to pass in the original props of the component through to this component  */}
+                    {/* <Component hovering={this.state.hovering} {...this.props} /> */}
+                    {/* the object spread passes the original components props through to this rendered component */}
+
+                    {/* new UI taking propName into account */}
+                    <Component {...props} />
+                </div>
+            )
+        }
+
+    }
+}
