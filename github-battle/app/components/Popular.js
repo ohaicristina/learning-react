2acddcd60a0ecbf1035970822bb179fa7c1e95b6
Cristina Silva <casilva62592@gmail.com>
2021-01-14 20:09:35 -0500

    Add tooltip to popular component

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -4,6 +4,7 @@ import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons
 import { fetchPopularRepos } from '../utils/api'
 import Card from './Card'
 import Loading from './Loading'
+import Tooltip from './Tooltip'
 
 function LanguagesNav(props) {
     const { selected, onUpdateLanguage } = props;
@@ -49,10 +50,12 @@ function ReposGrid({ repos }) {
                         >
                             <ul className="card-list">
                                 <li>
-                                    <FaUser color='rgb(255, 191, 116)' size={22} />
-                                    <a href={`https://github.com/${login}`}>
-                                        {login}
-                                    </a>
+                                    <Tooltip text="Github username">
+                                        <FaUser color='rgb(255, 191, 116)' size={22} />
+                                        <a href={`https://github.com/${login}`}>
+                                            {login}
+                                        </a>
+                                    </Tooltip>
                                 </li>
                                 <li>
                                     <FaStar color='rgb(255, 215, 0)' size={22} />
